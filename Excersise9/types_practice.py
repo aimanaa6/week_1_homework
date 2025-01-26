b3b368d639110703802443199831a9b5fe95512a
Aiman Ahmed <aimanahmed61@gmail.com>
2025-01-26 21:22:26 +0000

    CREATE: created type_practice.py EDIT: added names,list and dictionary

---
+++ Excersise9/types_practice.py
@@ -0,0 +1,14 @@
+first_name ='Aiman'
+last_name = 'Ahmed'
+print(first_name,last_name)
+
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+name_list = ['Aiman', 'Ahmed']
+print(name_list)
+
+names = {'first':'Aiman', 'last': 'Ahmed'}
+print(names)
+print(names['first'])
+print(names['last'])
