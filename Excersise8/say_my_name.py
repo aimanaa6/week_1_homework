768a292f12adbd03bf62aa7ac351b4597437e734
Aiman Ahmed <aimanahmed61@gmail.com>
2025-01-27 19:07:25 +0000

    EDIT: added comments to say_my_name.py

---
+++ Excersise8/say_my_name.py
@@ -0,0 +1,8 @@
+# first variable
+first_name ='Aiman'
+# second variable
+last_name = 'Ahmed'
+# generated both variables using print function (already produced a space)
+print(first_name,last_name)
+full_name = first_name + ' ' + last_name
+print(full_name)
