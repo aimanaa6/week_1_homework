259132e3d51cace34a62f483f117b1b32a6aa91b
Aiman Ahmed <aimanahmed61@gmail.com>
2025-01-27 20:31:40 +0000

    EDIT: equation amended

--- Excersise9/projectile.py
+++ Excersise9/projectile.py
@@ -10,12 +10,11 @@ v0 = 80
 # the horizontal distance travelled
 x = 0.5
 # elevation angle
-theta_deg = 80
 # converting degrees to radians
-theta = theta_deg * (pi/180)
+theta = 80 * (pi/180)
 # Height of a projectile (y) from a gun (ignoring air resistance)
 y = y0 + x * tan(theta) - (g * x**2) / (2 * (v0 * cos(theta))**2)
-print(f"The height of the projectile is: {y} meters")
+print(y)
 
 
 
