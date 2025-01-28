54990737ea70c661c98dd93776eb1d6f8eab82de
Pooja Murali <p.murali2002@gmail.com>
2025-01-28 18:46:22 +0000

    EDIT: added comments to types and projectiles

--- Excersise9/projectiles.py
+++ Excersise9/projectiles.py
@@ -1,9 +1,15 @@
+# import module
 from math import pi,tan,cos
+
+# set all the variables
 y0 = 1
 x = 0.5
 theta = 80 * (pi/180)
 v0 = 44
 g = 9.81
+
+# sub into the equation
 y = y0 + x * tan(theta) - g*(x**2)/ ((v0 * cos(theta))**2) * 2
 
+# print the answer
 print(y)
--- Excersise9/types_practice.py
+++ Excersise9/types_practice.py
@@ -1,11 +1,16 @@
+
+# variable 1
 firstname = 'Pooja'
+# variable 2
 lastname = 'Murali'
 print(firstname, lastname)
 
+# the function and syntax for a list
 names_list =['Pooja', 'Murali']
 print(type(names_list))
 print(names_list)
 
+# The function and syntax for a dictionary
 names_dictionary = {"first": "Pooja", "last": "Murali"}
 print(type(names_dictionary))
 print(names_dictionary)
